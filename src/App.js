18de582d20f7d0651ffaf9dec5dd6c4ba5152a2c
jesusbyte <jesusrodrigo881@gmail.com>
2021-11-13 02:04:06 -0600

    Folder components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="bg-red-500 h-screen">
-      <p>Funciona</p>
+      <p className="bg-blue-800 w">Funciona</p>
+      <Nav/>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Nav = () => {
+
+    return(
+        <h1>Title</h1>
+    )
+}
+
+export default Nav
