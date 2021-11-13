e1dde604ecfee3161ca6e2d687b6d3eb4e88f9e1
LedaHuerta <ledamhuerta@gmail.com>
2021-11-13 16:01:01 -0600

    implementando routing

---
+++ src/components/SurvivalGuide/SurvivalGuide.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SurvivalGuide = () => {
+  return (
+    <div>
+        <h2>esto es survival</h2>
+    </div>
+
+  );
+};
+export default SurvivalGuide;
---
+++ src/components/exampleRouting/FirstAid.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FirstAid = () => {
+  return (
+    <div>
+        <h2>Primeros Auxilios</h2>
+    </div>
+  );
+};
+export default FirstAid;
