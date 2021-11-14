e664183ee1f1399e0ffd8463c51c064c7a1fdfad
jesusbyte <jesusrodrigo881@gmail.com>
2021-11-13 20:00:35 -0600

    Nesting Route

--- src/components/firstAid/FisrtAid.js
+++ src/components/firstAid/FisrtAid.js
@@ -1,17 +1,22 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 const FisrtAid = () => {
+
   return (
-    <div class="flex flex-col w-full grid justify-items-center mt-14 ">
-      <div className="block my-4 w-3/4 flex justify-center content-center h-12">
-        <a className="m-auto">Primeros auxilios basicos</a>
-      </div>
-      <div className="block my-4 w-3/4 flex justify-center content-center h-12">
-        <h2 className="m-auto"> Medicamentos escenciales y su uso </h2>
-      </div>
-      <div className="block my-4 w-3/4 flex justify-center content-center h-12">
-        <h2 className="m-auto">Guia de Plantas</h2>
-      </div>
+    <div className="flex flex-col w-full grid justify-items-center mt-14 ">
+        <div className="block my-4 w-3/4 flex justify-center content-center h-12">
+          <Link to="/first-aid/1" className="m-auto">1 numero</Link>
+        </div>
+        <div className="block my-4 w-3/4 flex justify-center content-center h-12">
+          <Link to="/first-aid/2" className="m-auto">2 numero</Link>
+        </div>
+        <div className="block my-4 w-3/4 flex justify-center content-center h-12">
+          <Link to="/first-aid/3" className="m-auto">3 numero</Link>
+        </div>
+        <div className="block my-4 w-3/4 flex justify-center content-center h-12">
+          <h2 className="m-auto">Guia de Plantas</h2>
+        </div>
     </div>
   );
 };
